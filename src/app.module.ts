74d4f1ffaf56e396946ce3ddaee655564c6a8147
kasyap dharanikota <76957823+kasyap1234@users.noreply.github.com>
2023-08-06 00:09:35 +0530

    no erros as of now

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { QuizzesModule } from './quizzes/quizzes.module';
+
 import { PollingModule } from './polling/polling.module';
 import { UserModule } from './auth/user/user.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [QuizzesModule, PollingModule, UserModule, AuthModule],
+  imports: [PollingModule, UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/polling/polling.module.ts
+++ src/polling/polling.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PollingService } from './polling.service';
 import { PollingController } from './polling.controller';
+import { UserService } from '../auth/user/user.service'; 
 
 @Module({
-  providers: [PollingService],
+  
+  providers: [PollingService,UserService],
   controllers: [PollingController]
 })
 export class PollingModule {}
